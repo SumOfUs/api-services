cc5860249b3feba00e85e60733c3e9d3cda62703
Omar Sahyoun <osahyoun@gmail.com>
2017-11-17 13:15:18 +0000

    Extracts braintree client to its own file.

--- payments-service/providers/braintree/token.js
+++ payments-service/providers/braintree/token.js
@@ -1,11 +1,4 @@
-import braintree from 'braintree';
-
-const gateway = braintree.connect({
-  environment: braintree.Environment[process.env.BRAINTREE_ENV],
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-});
+import gateway from '../../../shared/clients/braintree';
 
 const response = (code, body) => {
   return {
