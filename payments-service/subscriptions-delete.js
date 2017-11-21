a7ac7d962fd3e77cee2e56bef2253fc6e096e493
Omar Sahyoun <osahyoun@gmail.com>
2017-11-21 17:10:40 +0000

    extra work on subscription cancellation workflow

--- payments-service/subscriptions-delete.js
+++ payments-service/subscriptions-delete.js
@@ -3,13 +3,9 @@ import braintree from '../shared/clients/braintree';
 import guid from 'guid';
 import AWS from 'aws-sdk';
 
-export const handler = (event, context, callback) => {
-  const id = event.pathParameters.id;
-  const documentClient = new AWS.DynamoDB.DocumentClient();
-  // braintree.subscription.cancel(id, (err, result) => {
-  //   console.log(JSON.stringify(result, null, 2));
-  // });
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
+const logOperation = id => {
   const params = {
     TableName: process.env.DB_LOG_TABLE,
     Item: {
@@ -17,42 +13,34 @@ export const handler = (event, context, callback) => {
       createdAt: new Date().toISOString(),
       eventType: 'PAYMENT_SERVICE:SUBSCRIPTION:CANCEL',
       data: {
-        recurring_id: id,
+        recurringId: id,
         paymentProcessor: 'braintree',
       },
       status: {
-        braintree: 'SUCCESS',
         actionkit: 'PENDING',
         champaign: 'PENDING',
       },
     },
   };
 
-  // example of scaning a dynamodb Table
-  // documentClient.scan({
-  //   TableName : process.env.DB_LOG_TABLE,
-  // }).promise()
-  // .then((data) => {
-  //   console.log(data.Items);
-  // })
-
   documentClient
     .put(params)
     .promise()
-    .then(
-      data => {
-        callback(
-          null,
-          ok({
-            body: {
-              message:
-                'Go Serverless Webpack (Ecma Script) v1.0! First module!',
-              event,
-            },
-          })
-        );
-      },
-      err => console.log(err)
-    )
-    .catch(err => console.log(err));
+    .then(resp => console.log('TABLE PUT RESPONSE:', resp))
+    .catch(err => console.log('TABLE PUT ERROR', err));
+};
+
+export const handler = (event, context, callback) => {
+  const id = event.pathParameters.id;
+
+  braintree.subscription
+    .cancel(id)
+    .then(() => {
+      logOperation(id);
+      callback(null, ok());
+    })
+    .catch(err => {
+      console.log('BRAINTREE SUBSCRIPTION CANCEL ERROR:', err);
+      callback(null, badRequest());
+    });
 };
---
+++ payments-service/updateOperationsLog.js
@@ -0,0 +1,20 @@
+export default (id, createdAt, state, platform) => {
+  const params = {
+    TableName: process.env.DB_LOG_TABLE,
+    Key: {
+      id: id,
+      createdAt: createdAt,
+    },
+    UpdateExpression: 'set #s.#platform = :state',
+    ExpressionAttributeNames: { '#s': 'status', '#platform': platform },
+    ExpressionAttributeValues: {
+      ':state': state,
+    },
+  };
+
+  documentClient
+    .update(params)
+    .promise()
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
+};
