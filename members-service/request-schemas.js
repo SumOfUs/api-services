a109f29cc1c79103d50338aa6a08a9f0148aa377
Vince Martinez <eykosioux@gmail.com>
2017-11-29 01:01:30 +0000

    Update schema for update member

--- members-service/request-schemas.js
+++ members-service/request-schemas.js
@@ -2,18 +2,9 @@ export const LIST_MEMBERS_SCHEMA = {
   type: 'object',
   required: ['email'],
   properties: {
-    email: {
-      type: 'string',
-      format: 'email',
-    },
-    last_name__iexact: {
-      type: 'string',
-      minLength: 2,
-    },
-    country__iexact: {
-      type: 'string',
-      minLength: 2,
-    },
+    email: { type: 'string', format: 'email' },
+    last_name__iexact: { type: 'string', minLength: 2 },
+    country__iexact: { type: 'string', minLength: 2 },
   },
 };
 
@@ -21,10 +12,7 @@ export const SHOW_MEMBER_SCHEMA = {
   type: 'object',
   required: ['id'],
   properties: {
-    id: {
-      type: 'string',
-      pattern: '^[0-9]+$',
-    },
+    id: { type: 'string', pattern: '^[0-9]+$' },
   },
 };
 
@@ -32,10 +20,12 @@ export const UPDATE_MEMBER_SCHEMA = {
   type: 'object',
   required: ['id'],
   properties: {
-    id: {
-      type: 'string',
-      pattern: '^[0-9]+$',
-    },
+    id: { type: 'string', pattern: '^[0-9]+$' },
+    email: { type: 'string', format: 'email' },
+    first_name: { type: 'string', minLength: 2 },
+    last_name: { type: 'string', minLength: 2 },
+    country: { type: 'string', minLength: 2 },
+    postal: { type: 'string', minLength: 2 },
   },
 };
 
@@ -43,9 +33,6 @@ export const UNSUBSCRIBE_MEMBER_SCHEMA = {
   type: 'object',
   required: ['id'],
   properties: {
-    id: {
-      type: 'string',
-      pattern: '^[0-9]+$',
-    },
+    id: { type: 'string', pattern: '^[0-9]+$' },
   },
 };
