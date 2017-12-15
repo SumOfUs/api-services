2ac4aa3fcf1839b4490b3c38d99e0457238eb08a
tuuli <tuuli.pollanen@gmail.com>
2017-12-15 12:12:03 +0100

    subscription cancellation specs or bt and gocardless

--- payments-service/subscriptions.test.js
+++ payments-service/subscriptions.test.js
@@ -1,15 +1,10 @@
 // @flow weak
 
-import {
-  cancelSubscription,
-  logOperation,
-  gocardless,
-  handler,
-} from './subscriptions-delete';
+import { cancelSubscription, handler } from './subscriptions-delete';
 import { client } from '../lib/clients/braintree/braintree';
 
 describe('handler', () => {
-  describe('Deleting a subscription', () => {
+  describe('Cancelling a Braintree subscription', () => {
     const cb = jest.fn(function(err, response) {
       return response;
     });
@@ -50,6 +45,51 @@ describe('handler', () => {
             body: expect.stringContaining(
               'Subscription has already been canceled.'
             ),
+          })
+        );
+      });
+    });
+  });
+
+  describe('Cancelling a GoCardless subscription', () => {
+    const cb = jest.fn(function(err, response) {
+      return response;
+    });
+
+    describe('Success', () => {
+      const event = {
+        pathParameters: {
+          id: 'SB0000BPD0CBDC',
+          provider: 'gocardless',
+        },
+      };
+
+      test('passes the right parameters to cancelSubscription', () => {
+        const fn = jest.fn((...params) => Promise.resolve(params));
+        handler(event, null, cb, fn);
+        expect(fn).toBeCalledWith('SB0000BPD0CBDC', 'gocardless');
+      });
+
+      test('responds on success (replayer)', () => {
+        return expect(handler(event, null, cb)).resolves.toEqual(
+          expect.objectContaining({ statusCode: 200, body: '' })
+        );
+      });
+    });
+
+    describe('Failure', () => {
+      const event = {
+        pathParameters: {
+          id: '',
+          provider: 'gocardless',
+        },
+      };
+
+      test('responds on errors / exceptions (replayer)', () => {
+        return expect(handler(event, null, cb)).resolves.toEqual(
+          expect.objectContaining({
+            statusCode: 400,
+            body: expect.stringContaining('not found'),
           })
         );
       });
