c728ad25e8cd1b1758cf6711f2d88a4a9c38e947
Vince Martinez <eykosioux@gmail.com>
2017-12-15 15:13:21 +0000

    [wip] logger stuff

---
+++ payments-service/subscriptions-delete-actionkit.test.js
@@ -0,0 +1,69 @@
+import AWS from 'aws-sdk';
+import { marshall, unmarshall } from 'aws-sdk/lib/dynamodb/converter';
+import { handler } from './subscriptions-delete-actionkit';
+import { OperationsLogger } from '../lib/dynamodb/operationsLogger';
+import { CANCEL_PAYMENT_EVENT } from '../lib/dynamodb/eventTypeChecker';
+import uuidv1 from 'uuid/v1';
+
+jest.spyOn(AWS.DynamoDB.DocumentClient.prototype, 'update');
+jest.spyOn(OperationsLogger.prototype, 'updateStatus');
+jest.spyOn(OperationsLogger.prototype, 'log');
+
+const validEvent = {
+  Records: [
+    {
+      dynamodb: {
+        NewImage: marshall({
+          eventName: 'INSERT',
+          eventType: CANCEL_PAYMENT_EVENT,
+          id: uuidv1(),
+          createdAt: new Date().toISOString(),
+          status: { actionkit: 'PENDING', champaign: 'PENDING' },
+          data: {
+            recurringId: 'testRecurringId',
+          },
+        }),
+      },
+    },
+  ],
+};
+describe('subscriptions-delete-actionkit handler', function() {
+  test('basically, it is a function', () => {
+    expect(typeof handler).toEqual('function');
+  });
+
+  test('returns if the event is not a cancel payment event', () => {
+    const event = {
+      Records: [
+        { dynamodb: { NewImage: { eventName: 'INSERT', eventType: 'TEST' } } },
+      ],
+    };
+    const cb = jest.fn();
+    handler(event, null, cb);
+    expect(cb).toHaveBeenCalledWith(null, 'Not a cancel event');
+  });
+
+  test('attempts to cancel the record', () => {
+    const record = unmarshall(validEvent.Records[0].dynamodb.NewImage);
+    const cb = jest.fn();
+    const cancelFn = jest.fn(() => Promise.resolve());
+    handler(validEvent, null, cb, cancelFn);
+    expect(cancelFn).toHaveBeenCalledWith(record.data.recurringId);
+  });
+
+  test('on AK success, updates the operation log with success', () => {
+    const record = unmarshall(validEvent.Records[0].dynamodb.NewImage);
+    const cb = jest.fn();
+    const cancelFn = jest.fn(() => Promise.resolve());
+    handler(validEvent, null, cb, cancelFn);
+
+    expect(OperationsLogger.prototype.updateStatus).toHaveBeenCalledWith({
+      data: { hello: 'world' },
+    });
+
+    // expect(OperationsLogger.prototype.updateStatus).toHaveBeenCalledWith(
+    //   record,
+    //   { actionkit: 'SUCCESS' }
+    // );
+  });
+});
---
+++ payments-service/subscriptions-delete-champaign.test.js
@@ -0,0 +1,3 @@
+import { handler } from './subscriptions-delete-champaign';
+
+test('true', () => expect(typeof handler).toBe('function'));
