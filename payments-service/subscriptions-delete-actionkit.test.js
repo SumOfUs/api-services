f4ea5b75576392f8e15970425a4970eb4beecd06
Vince Martinez <eykosioux@gmail.com>
2017-12-16 01:12:30 +0000

    Increase test coverage

--- payments-service/subscriptions-delete-actionkit.test.js
+++ payments-service/subscriptions-delete-actionkit.test.js
@@ -5,61 +5,71 @@ import { OperationsLogger } from '../lib/dynamodb/operationsLogger';
 import { CANCEL_PAYMENT_EVENT } from '../lib/dynamodb/eventTypeChecker';
 import uuidv1 from 'uuid/v1';
 
-jest.spyOn(AWS.DynamoDB.DocumentClient.prototype, 'update');
-jest.spyOn(OperationsLogger.prototype, 'updateStatus');
-
-const validEvent = {
-  Records: [
-    {
-      dynamodb: {
-        NewImage: marshall({
-          eventName: 'INSERT',
-          eventType: CANCEL_PAYMENT_EVENT,
-          id: uuidv1(),
-          createdAt: new Date().toISOString(),
-          status: { actionkit: 'PENDING', champaign: 'PENDING' },
-          data: {
-            recurringId: 'testRecurringId',
-          },
-        }),
-      },
-    },
-  ],
-};
+const updateSpy = jest.spyOn(AWS.DynamoDB.DocumentClient.prototype, 'update');
+const statusSpy = jest.spyOn(OperationsLogger.prototype, 'updateStatus');
 
 describe('subscriptions-delete-actionkit handler', function() {
+  const updateStatusSpy = OperationsLogger.prototype.updateStatus;
   test('basically, it is a function', () => {
     expect(typeof handler).toEqual('function');
   });
 
-  test('returns if the event is not a cancel payment event', () => {
-    const event = {
-      Records: [
-        { dynamodb: { NewImage: { eventName: 'INSERT', eventType: 'TEST' } } },
-      ],
-    };
+  test('returns if the event is not a cancel payment event', async () => {
+    const event = invalidEvent(new Date().toISOString());
     const cb = jest.fn();
-    handler(event, null, cb);
+    await handler(event, null, cb);
     expect(cb).toHaveBeenCalledWith(null, 'Not a cancel event');
   });
 
-  test('attempts to cancel the record', () => {
-    const record = unmarshall(validEvent.Records[0].dynamodb.NewImage);
+  test('attempts to cancel the record', async () => {
+    const event = validEvent(new Date().toISOString());
+    const record = unmarshall(event.Records[0].dynamodb.NewImage);
     const cb = jest.fn();
     const cancelFn = jest.fn(() => Promise.resolve());
-    handler(validEvent, null, cb, cancelFn);
+    await handler(event, null, cb, cancelFn);
     expect(cancelFn).toHaveBeenCalledWith(record.data.recurringId);
   });
 
-  test('on AK success, updates the operation log with success', () => {
-    const record = unmarshall(validEvent.Records[0].dynamodb.NewImage);
-    const cb = jest.fn();
-    const cancelFn = jest.fn(() => Promise.resolve());
-    handler(validEvent, null, cb, cancelFn);
+  test(`[on ak success] updates the operation log status with 'success'`, async () => {
+    const event = validEvent(new Date().toISOString());
+    const record = unmarshall(event.Records[0].dynamodb.NewImage);
+    await handler(event, null, jest.fn(), () => Promise.resolve());
+    expect(statusSpy).toBeCalledWith(record, { actionkit: 'SUCCESS' });
+  });
 
-    expect(OperationsLogger.prototype.updateStatus).toHaveBeenCalledWith(
-      record,
-      { actionkit: 'SUCCESS' }
-    );
+  test(`[on ak failure] updates the operation log status with 'failure'`, async () => {
+    const event = validEvent(new Date().toISOString());
+    const record = unmarshall(event.Records[0].dynamodb.NewImage);
+    await handler(event, null, jest.fn(), () => Promise.reject());
+    expect(statusSpy).toBeCalledWith(record, { actionkit: 'FAILURE' });
   });
 });
+
+function validEvent(date) {
+  return {
+    Records: [
+      {
+        dynamodb: {
+          NewImage: marshall({
+            eventName: 'INSERT',
+            eventType: CANCEL_PAYMENT_EVENT,
+            id: uuidv1(),
+            createdAt: date,
+            status: { actionkit: 'PENDING', champaign: 'PENDING' },
+            data: {
+              recurringId: 'testRecurringId',
+            },
+          }),
+        },
+      },
+    ],
+  };
+}
+
+function invalidEvent() {
+  return {
+    Records: [
+      { dynamodb: { NewImage: { eventName: 'INSERT', eventType: 'INVALID' } } },
+    ],
+  };
+}
