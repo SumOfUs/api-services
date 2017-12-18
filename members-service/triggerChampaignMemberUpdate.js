cce5f493bf6a66304aa488941e6c2796499a7239
Vince Martinez <eykosioux@gmail.com>
2017-12-18 13:00:43 +0000

    pass client to operations logger

--- members-service/triggerChampaignMemberUpdate.js
+++ members-service/triggerChampaignMemberUpdate.js
@@ -1,11 +1,12 @@
 // @flow weak
 import { OperationsLogger } from '../lib/dynamodb/operationsLogger';
 import { updateMember } from '../lib/clients/champaign/member';
-import { DynamoDBStreams } from 'aws-sdk';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 const logger = new OperationsLogger({
   namespace: 'MEMBERS',
   tableName: process.env.DB_LOG_TABLE || '',
+  client: new DocumentClient(),
 });
 
 export function handler(e, ctx, cb) {
